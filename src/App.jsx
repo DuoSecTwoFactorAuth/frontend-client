4fac0d1dc90e996b6b323403c17fc83d47098bf0
Anjali <anjali184a@gmail.com>
2022-12-09 23:12:20 +0530

    qr code page

--- src/App.jsx
+++ src/App.jsx
@@ -7,22 +7,24 @@ import RegisterPageII from "./pages/RegisterPageII.jsx";
 import Dashboard from "./pages/Dashboard.jsx";
 import Settings from "./pages/Settings.jsx";
 import Navbar from "./components/Navbar.jsx";
+import QRCodePage from "./pages/QRCode.jsx";
 
 function App() {
   return (
     <Routes>
-        <Route path="/">
-          <Route index element={<LandingPage />} />
-          <Route path="registration-basic-details" element={<RegisterPageI />} />
-          <Route path="registration-full-details" element={<RegisterPageII />} />
-          <Route path="login" element={<LoginPage />} />
-          <Route path="company" element={<Navbar />}>
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="settings" element={<Settings />} />
-          </Route>
+      <Route path="/">
+        <Route index element={<LandingPage />} />
+        <Route path="registration-basic-details" element={<RegisterPageI />} />
+        <Route path="registration-full-details" element={<RegisterPageII />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="company" element={<Navbar />}>
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="settings" element={<Settings />} />
         </Route>
+        <Route path="qr-code/:companyEmployeeHash" element={<QRCodePage />} />
+      </Route>
     </Routes>
-  )
+  );
 }
 
 export default App;
